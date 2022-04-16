9ba24322f7e4fae9b500e137dc31878804444134
craftingcity <ianjwuth@gmail.com>
2022-04-15 23:53:56 -0700

    pushing some blessed examples

--- testingOne.py
+++ testingOne.py
@@ -2,13 +2,21 @@ from blessed import *
 from json import *
 
 def main():
-	terminal = Terminal()
-	data = terminal.black_on_white("A")
-	for i in range(0, (terminal.height // 2) - 5):
-		for i in range(0, (terminal.width // 2) - 5):
-			print(data, end='')
-		print()
-
+    terminal = Terminal()
+    
+    TERM_HEIGHT = terminal.height
+    TERM_WIDTH = terminal.width
+    
+    
+    for i in range(0, TERM_HEIGHT // 4):
+        for i in range(0, TERM_WIDTH // 4):
+            print(terminal.white_on_blue("."), end='')
+    
+    terminal.home()
+    print(terminal.white_on_green("This is home."), end='')
+    terminal.moveup(2)
+    terminal.moveright(4)
+    print(terminal.white_on_red("This is home, up 2, right 4."), end='')
 
 
 if __name__ == "__main__":
