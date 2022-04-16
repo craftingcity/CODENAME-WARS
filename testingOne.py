8bb141ab70e9cc0ae8589b265cb8fae861189b30
no <ianjwuth@gmail.com>
2022-04-15 21:44:44 -0700

    finalized initial toolset
    use cmd, notepad++ for live runs
    use blessed as library

--- testingOne.py
+++ testingOne.py
@@ -1,1 +1,15 @@
 from blessed import *
+from json import *
+
+def main():
+	terminal = Terminal()
+	data = terminal.black_on_white("A")
+	for i in range(0, (terminal.height // 2) - 5):
+		for i in range(0, (terminal.width // 2) - 5):
+			print(data, end='')
+		print()
+
+
+
+if __name__ == "__main__":
+	main()
