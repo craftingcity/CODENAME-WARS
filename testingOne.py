54ed93c78d324756bd0c7e61ffe4bd7002b7fcad
craftingcity <ianjwuth@gmail.com>
2022-04-19 01:28:28 -0700

    fuck if i know it stopped working again

--- testingOne.py
+++ testingOne.py
@@ -1,13 +1,11 @@
 import curses
 import json
 
-for i in dir(curses):
-    print(i)
 
 
-def main():
-    print("Go for W.A.R.S Alpha One")
+print("Go for W.A.R.S Alpha One")
+print("Initializing Screen...")
+fullscreen = curses.initscr()
 
 
-if __name__ == "__main__":
-    main()
+
