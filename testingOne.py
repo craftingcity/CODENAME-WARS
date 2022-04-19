d58e6636b899f71bf34bc11532028a3cdfe02db0
craftingcity <ianjwuth@gmail.com>
2022-04-19 01:02:17 -0700

    FIX CIRCULAR IMPORT
    use an ide. lol.
    
    i picked pycharm

--- testingOne.py
+++ testingOne.py
@@ -1,23 +1,13 @@
-from blessed import *
-from json import *
+import curses
+import json
+
+for i in dir(curses):
+    print(i)
+
 
 def main():
-    terminal = Terminal()
-    
-    TERM_HEIGHT = terminal.height
-    TERM_WIDTH = terminal.width
-    
-    
-    for i in range(0, TERM_HEIGHT // 4):
-        for i in range(0, TERM_WIDTH // 4):
-            print(terminal.white_on_blue("."), end='')
-    
-    terminal.home()
-    print(terminal.white_on_green("This is home."), end='')
-    terminal.moveup(2)
-    terminal.moveright(4)
-    print(terminal.white_on_red("This is home, up 2, right 4."), end='')
+    print("Go for W.A.R.S Alpha One")
 
 
 if __name__ == "__main__":
-	main()
+    main()
