39591446d49f273572ddead6d7850d2d4505bced
craftingcity <ianjwuth@gmail.com>
2022-04-26 17:19:47 -0700

    added more testingOne methods

--- testingOne.py
+++ testingOne.py
@@ -64,6 +64,63 @@ def basic_movement(stdscr):
         
 
 
+def rebindingv1(stdscr):
+    ## initialize vars
+    current_key = ""
+    bound_key = ""
+    rebind_key = "="
+    counter = 0
+
+    ## initialize looping
+    while True:
+        if current_key == "q":
+            break
+        
+        ## screen manipulation 
+        stdscr.clear()
+        stdscr.addstr(10, 10, f"Your last pressed key is: {current_key} The currently bound key is: {bound_key} Press {rebind_key} to rebind.")
+        stdscr.addstr(11, 10, f"Counter: {counter}")
+        stdscr.refresh()
+
+        ## wait here for input; either we increase the count OR rebind a key OR pass 
+        current_key = stdscr.getkey()
+        
+        if current_key == bound_key:
+            counter += 1
+        if current_key == rebind_key:
+            ## screen manipulation; ask for new input
+            stdscr.clear()
+            stdscr.addstr(10, 10, "Press new key...")
+            stdscr.refresh()
+            ## edit variables - make sure there is no overwriting
+            current_key = stdscr.getkey()
+            if current_key in [bound_key, rebind_key]:
+                stdscr.clear()
+                stdscr.addstr(10, 10, "Rebind failed; key in use. Please try again soon!")
+                stdscr.refresh()
+                time.sleep(0.4)
+            else:
+                bound_key = current_key
+
+def menuv1(stdscr):
+    ## initialize working vars
+    count = 0
+    spacer = 4
+    ## populate option array
+    option_arry = ["Option One", "Option Two", "Bingoboard", "Option Four"]
+    ## display options
+    stdscr.clear()
+    for i in option_arry:
+        stdscr.addstr(0, count, i)
+        count += 1
+    stdscr.refresh()
+
+
+    ## initialize input handler
+    c = stdscr.getkey()
+
+    
+
 
 ## wrapper calls main, giving use of the entire terminal as stdscr
-wrapper(basic_movement)
+wrapper(menuv1)
---
+++ wars_run_file.py
@@ -0,0 +1,11 @@
+import curses
+from curses import wrapper
+import json
+
+def main(stdscr):
+    
+
+
+
+if __name__ == "__main__":
+    wrapper(main)
